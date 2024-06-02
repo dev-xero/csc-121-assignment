efb56e34da22beae8c668acb5bd7aa299de79740
dev-xero <elo48858@gmail.com>
2024-06-02 15:04:43 +0100

    feat: Question 3.31 b

---
+++ src/ex-3.31-b.c
@@ -0,0 +1,22 @@
+/** Header files */
+#include <stdio.h>
+
+/** Exercise 3.31 - b */
+int main(void) {
+    int x = 5;
+    int y = 8;
+
+    if (y == 8)
+        puts("@@@@@");
+    
+    if (x == 5) {}
+
+    else
+    {
+        puts("$$$$$");
+        puts("#####");
+        puts("&&&&&");   
+    }
+
+    return 0;
+}
