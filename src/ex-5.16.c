cab1d4a7b8c68f4a0dc58d6f6c9cca2fbed5eddf
dev-xero <elo48858@gmail.com>
2024-06-02 16:28:27 +0100

    feat: Question 5.16

---
+++ src/ex-5.16.c
@@ -0,0 +1,44 @@
+/** Header files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/** Computes the area of a triangle using Heron's formula 
+ * 
+ * Area = sqrt(s * (s - a) * (s - b) * (s - c))
+ * where s: is the semi perimeter = (a + b + c) / 2
+*/
+double area(double a, double b, double c)
+{
+    /**
+     * Check that sides are those of a triangle
+     * if the sum of two sides is less than the third side, it is not a triangle
+    */
+    if (((a + b) < c) || ((a + c) < b) || ((b + c) < a))
+    {
+        printf("Sides do not represent those of a triangle.");
+        exit(1);
+    }
+
+    // Calculate semi perimeter
+    int s = (a + b + c) / 2;
+
+    return sqrt(s*(s-a)*(s-b)*(s-c));
+}
+
+/** C program to compute the area of a triangle given 3 sides */
+int main(void)
+{
+    double side_a, side_b, side_c;
+    double computed_area = 0.0;
+
+    printf("%s", "Enter the sides of the triangle as: a, b, c: ");
+    scanf("%lf, %lf, %lf", &side_a, &side_b, &side_c);
+
+    printf("Side A: %.2lf units\nSide B: %.2lf units\nSide C: %.2lf units", side_a, side_b, side_c);
+
+    computed_area = area(side_a, side_b, side_c);
+    printf("\nArea of the triangle is: %.2lf sq. units", computed_area);
+
+    return 0;
+}
